7a89f9c081a46561443ef377e19edf20a6d9fca3
amitme0014 <amit.aq.singh@capgemini.com>
2017-07-24 11:08:00 +0530

    ScriptBucket
    This script will make an s3 storage and then uploads the file on the basis of given path.

---
+++ amit.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+echo "existing buckets are"
+aws s3 ls
+read -p "Enter the bucket name : " buckName
+comp=$(aws s3 ls | grep -o "$buckName")
+if [[ $buckName = $comp ]]; then
+	echo "There is already something in the bucket from this name.Are you sure u want to add it??"
+	read -p "Enter yes to overwrite and no to again enter the bucket name:" permission
+	     if [["yes" == $permission]]; then
+	echo "Hii your bucket is trying to get prepared"
+	read -p "Enter the path of the file to b uploaded : " pathName
+    bucket=$(aws s3api create-bucket --bucket $buckName --region us-east-1)
+    echo $bucket
+    aws s3 cp $pathName s3://$buckName/
+    echo "file uploaded succesfully"
+         elif [[ "no" = $permission ]]; then
+	read -p "Enter the bucket name : " buckName
+	echo "Hii your bucket is trying to get prepared"
+	read -p "Enter the path of the file to b uploaded : " pathName
+    bucket=$(aws s3api create-bucket --bucket $buckName --region us-east-1)
+    echo $bucket
+    aws s3 cp $pathName s3://$buckName/
+    list=$(aws s3 ls s3://$buckname)	
+    echo $list
+	     else
+	echo "invalid choice"	 
+	      fi  
+ else 
+	#read -p "Enter the bucket name : " buckName
+	echo "Hii your bucket is trying to get prepared"
+	read -p "Enter the path of the file to b uploaded : " pathName
+    bucket=$(aws s3api create-bucket --bucket $buckName --region us-east-1)
+    echo $bucket
+    aws s3 cp $pathName s3://$buckName/
+	aws s3 ls s3://$buckName --recursive --human-readable --summarize
+fi
